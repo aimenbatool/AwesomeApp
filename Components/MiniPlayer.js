89c6266fc4e14c23f1c01ac0478168cd86dfa0ae
Aimen Batool <s.aimenbatool@gmail.com>
2021-04-14 22:21:28 +0500

    Fix mini player css

--- Components/MiniPlayer.js
+++ Components/MiniPlayer.js
@@ -15,62 +15,54 @@ const MiniPlayer = () => {
   return (
     <TouchableOpacity onPress={() => navigation.navigate('PlayerScreen')}>
       <Container style={styles.miniPlayer}>
-        <Content>
-          <View style={styles.trackDetails}>
-            <View>
-              <Thumbnail
-                style={styles.thumbnail}
-                square
-                source={{uri: playerContext.currentTrack.artwork}}
-              />
-            </View>
-
-            <View style={styles.titleContainer}>
-              <Text numberOfLines={1} style={styles.trackTitle}>
-                {playerContext.currentTrack.title}
-              </Text>
-              <Text numberOfLines={1} style={styles.category}>
-                {playerContext.currentTrack.album}
-              </Text>
-            </View>
-
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity onPress={() => playerContext.seekTo(-10)}>
-                <Icon
-                  style={styles.actionButton}
-                  name="md-play-forward-outline"
-                  size={1}
-                />
-              </TouchableOpacity>
-
-              {playerContext.isPaused ? (
-                <TouchableOpacity onPress={() => playerContext.play()}>
-                  <Icon
-                    style={styles.actionButton}
-                    name="play-circle-outline"
-                    size={1}
-                  />
-                </TouchableOpacity>
-              ) : (
-                <TouchableOpacity onPress={() => playerContext.pause()}>
-                  <Icon
-                    style={styles.actionButton}
-                    name="pause-circle-outline"
-                    size={1}
-                  />
-                </TouchableOpacity>
-              )}
-
-              <TouchableOpacity onPress={() => playerContext.seekTo(+10)}>
-                <Icon
-                  style={styles.actionButton}
-                  name="md-play-back-outline"
-                  size={1}
-                />
-              </TouchableOpacity>
-            </View>
+        <View>
+          <Thumbnail source={{uri: playerContext.currentTrack.artwork}} />
+        </View>
+        <View style={styles.trackDetails}>
+          <View style={styles.titleContainer}>
+            <Text numberOfLines={1} style={styles.trackTitle}>
+              {playerContext.currentTrack.title}
+            </Text>
+            <Text numberOfLines={1} style={styles.category}>
+              {playerContext.currentTrack.album}
+            </Text>
           </View>
-        </Content>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={() => playerContext.seekTo(-10)}>
+              <Icon
+                style={styles.actionButton}
+                name="md-play-forward-outline"
+                size={1}
+              />
+            </TouchableOpacity>
+
+            {playerContext.isPaused ? (
+              <TouchableOpacity onPress={() => playerContext.play()}>
+                <Icon
+                  style={styles.actionButton}
+                  name="play-circle-outline"
+                  size={1}
+                />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity onPress={() => playerContext.pause()}>
+                <Icon
+                  style={styles.actionButton}
+                  name="pause-circle-outline"
+                  size={1}
+                />
+              </TouchableOpacity>
+            )}
+
+            <TouchableOpacity onPress={() => playerContext.seekTo(+10)}>
+              <Icon
+                style={styles.actionButton}
+                name="md-play-back-outline"
+                size={1}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
       </Container>
     </TouchableOpacity>
   );
@@ -100,16 +92,21 @@ let styles = StyleSheet.create({
   trackDetails: {
     flexDirection: 'row',
     alignItems: 'center',
-    // justifyContent: 'space-between',
+    borderColor: 'red',
+    width: '82%',
+    justifyContent: 'space-between',
   },
   thumbnail: {
     borderRadius: 6,
   },
   miniPlayer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'space-between',
     width: '100%',
-    height: '40%',
+    height: 80,
     backgroundColor: '#F5FAFA',
-    // padding: 6,
+    paddingLeft: 12,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
