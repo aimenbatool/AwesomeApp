9b66176dcca43df1fcd878431bed66e168e6cc96
Aimen Batool <s.aimenbatool@gmail.com>
2022-04-20 00:04:16 +0500

    Fix about us screen issues

--- Navigators/DrawerContent.js
+++ Navigators/DrawerContent.js
@@ -1,49 +1,49 @@
-import {View, Text, StyleSheet, Image } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import React from 'react';
-import {DrawerContentScrollView, DrawerItem , DrawerItemList} from '@react-navigation/drawer';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Icon1 from 'react-native-vector-icons/Entypo';
-
-import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
-
 
 const DrawerContent = props => {
   return (
     <View style={{flex: 1}}>
-      <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:"#6497b1" , padding:30}}>
-    
-    <Image source={require('../assets/images/logo.jpeg')} 
-    style={{ height:90 , width:90 , borderRadius:100/2 , alignSelf:"center" , marginVertical:20}}></Image>
-    <Text style={{ fontSize:18 , alignSelf:"center" , color:"white"}}> Ghaderi.pk </Text>
-
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{backgroundColor: '#6497b1', padding: 30}}>
+        <Image
+          source={require('../assets/images/logo.jpeg')}
+          style={{
+            height: 90,
+            width: 90,
+            borderRadius: 100 / 2,
+            alignSelf: 'center',
+            marginVertical: 20,
+          }}
+        />
+        <Text style={{fontSize: 18, alignSelf: 'center', color: 'white'}}>
+          Ghaderi.pk
+        </Text>
       </DrawerContentScrollView>
 
       <View style={styles.drawerSection}>
-        
         <DrawerItem
-          icon={() => (
-            <Icon name="book" color={'#6497b1'} size={28} />
-          )}
+          icon={() => <Icon name="book" color={'#6497b1'} size={28} />}
           label="ہمارے بارے میں "
-          labelStyle={{fontSize:20 , fontFamily:"JameelNooriRegular" }}
+          labelStyle={{fontSize: 20, fontFamily: 'JameelNooriRegular'}}
           activeBackgroundColor="blue"
           onPress={() => {
             props.navigation.navigate('About');
           }}
         />
         <DrawerItem
-          icon={() => (
-            <Icon1 name="phone" color={'#6497b1'} size={30} />
-          )}
+          icon={() => <Icon1 name="phone" color={'#6497b1'} size={30} />}
           label="رابطہ کریں"
-          labelStyle={{fontSize:20, fontFamily:"JameelNooriRegular"}}
+          labelStyle={{fontSize: 20, fontFamily: 'JameelNooriRegular'}}
           onPress={() => {
             props.navigation.navigate('Contact');
           }}
         />
-        </View>
-        
-      
+      </View>
     </View>
   );
 };
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
     marginRight: 3,
   },
   drawerSection: {
-    marginBottom:400
+    marginBottom: 400,
   },
   bottomDrawerSection: {
     marginBottom: 15,
